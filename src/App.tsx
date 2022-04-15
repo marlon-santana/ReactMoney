e13ac9d7803b39ce62567d8b40390a80721308d8
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-04-15 01:23:18 -0300

    estrutura da aplicacao

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Title = styled.h1 `
+color: purple;
+font-size:50px ;
+`
+
+export function App() {
+  return (
+    
+    <Title>react money</Title>
+   
+    
+  )
+    
+}
+
+
