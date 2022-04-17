92a2975da1c8fabda750a87427216dca48798b6b
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-04-16 23:43:21 -0300

    add miragejs api fake

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import { Dashboard } from "./conponents/dashboard";
 import { Header } from "./conponents/Header";
+import { createServer  } from "miragejs";
 
-
-
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          categori: 'food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
--- src/conponents/TransactionsTable/index.tsx
+++ src/conponents/TransactionsTable/index.tsx
@@ -1,5 +1,49 @@
+import { useEffect } from 'react';
+import { Container } from './style';
+
+
+
 export function TransactionsTable () {
+    useEffect(() => { 
+        fetch('http://localhost:3000/api/transactions')
+        .then(response => response.json)
+        .then(data => console.log(data))
+    
+    },[])
+
+
     return (
-        <h1>transactions tables</h1>
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                    <tr>
+                        <td>notebook</td>
+                        <td className='withdraw'>R$500</td>
+                        <td>compras</td>
+                        <td>16/04/2021</td>
+                    </tr>
+
+                    <tr>
+                        <td>Desenvolvimento</td>
+                        <td className='deposit'>R$1.000</td>
+                        <td>serviços</td>
+                        <td>20/04/2021</td>
+                    </tr>
+
+                    <tr>
+                        <td>internet</td>
+                        <td className='withdraw'>R$200</td>
+                        <td>contas</td>
+                        <td>30/04/2021</td>
+                    </tr>
+                </thead>
+            </table>
+        </Container>
     )
 }
