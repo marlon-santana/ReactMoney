56fa2abdc8f2bf5cf69e4d481c7004dd154585b3
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-04-17 22:59:04 -0300

    add react-modal

--- src/conponents/Header/index.tsx
+++ src/conponents/Header/index.tsx
@@ -1,16 +1,34 @@
 import LogoImg from '../../assets/logo.svg'
 import { Container,Content } from './style';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 
 export function Header () {
+
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+      }
+
+      function closeModal() {
+        setIsOpen(false);
+      }
 return (
     <Container>
     <Content>
     <img src={LogoImg}  alt="rocketmoney" />
-    <button>Nova Transação</button>
+    <button onClick={() => openModal()}>Nova Transação</button>
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+      >
+          <h2>modal aberto</h2>
+      </Modal>
     </Content>
     </Container>
-    
+
 )
 
 }
