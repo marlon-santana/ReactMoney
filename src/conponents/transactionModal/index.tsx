c08461756edeca9b168cdf1273ab7f9be1c2a852
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-04-21 10:57:52 -0300

    estrutura do modal

--- src/conponents/transactionModal/index.tsx
+++ src/conponents/transactionModal/index.tsx
@@ -1,8 +1,9 @@
-import  { Container } from './style';
+import  { Container, ContainerButtons, RadioBox } from './style';
 import closeIcon from "../../assets/close.svg";
 import incomeIcon from "../../assets/income.svg";
 import outcomeIcon from "../../assets/outcome.svg";
 import Modal from 'react-modal';
+import { useState } from 'react';
 
 
 interface TransactionsModalProps {
@@ -11,19 +12,16 @@ interface TransactionsModalProps {
 }
 
 export function TransactionModal ({isOpen, onRequestClose}:TransactionsModalProps) {
+
+    const [type, setType ] =  useState('deposit')
     return (
         
         <Modal 
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName ='react-modal-overlay'
+            className = 'react-modal-content'
         >
-            <h2>Cadastrar transações</h2>
-            <Container>
-        <input 
-        placeholder='nome'/>
-
-        <input 
-        placeholder='preço'/>
 
         <button
         type="button"
@@ -32,13 +30,51 @@ export function TransactionModal ({isOpen, onRequestClose}:TransactionsModalProp
       >
         <img src={closeIcon} alt="Fechar modal" />
       </button>
-      
-        <button>Saida</button>
+      <Container>
+
+      <h2>Cadastrar transações</h2>
+            
+            <input 
+            placeholder='Titulo'/>
+    
+            <input 
+            type='number'
+            placeholder='preço'/>
+
+        <ContainerButtons>
+        <RadioBox
+            type="button"
+            isActive={type === "deposit"}
+            onClick={() => {
+              setType("deposit");
+            }}
+            activeColor="green"
+          >
+            <img src={incomeIcon} alt="Entradas" />
+            <span>Entradas</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeIcon} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+
+        </ContainerButtons>
+       
 
         <input 
         placeholder='categoria'/>
 
-        <button>Cadastrar</button>
+        <button
+        type='submit'
+        >Cadastrar</button>
         </Container>
 
         </Modal>
--- src/conponents/transactionModal/style.ts
+++ src/conponents/transactionModal/style.ts
@@ -1,7 +1,16 @@
 import styled from "styled-components";
+import { darken, transparentize } from 'polished'
 
-export const Container = styled.div`
-
+export const Container = styled.form`
+ 
+ *  {
+   display: flex;
+   flex-direction: row;
+   align-items: center;
+   padding: 0.3rem;
+   justify-content: center;
+ }
+  
   h2 {
     color: var(--text-title);
     font-size: 1.5rem;
@@ -39,5 +48,48 @@ export const Container = styled.div`
       filter: brightness(0.9);
     }
   }
+`
+export const ContainerButtons = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red'
+}
+
+const colors = {
+  green: '#33cc95',
+  red: '#e52e4d'
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: ${(props) => props.isActive 
+    ? transparentize(0.7,colors[props.activeColor]) 
+    : 'transparent'};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')}
+  }
+  img {
+    width: 30px;
+    height: 30px;
+  }
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+  
 `
 
